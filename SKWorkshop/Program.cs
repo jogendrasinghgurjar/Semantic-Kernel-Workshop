190a1e699ae9db308b35796654e95e81e10822aa
Prakhar Mishra <pramishra@microsoft.com>
2023-08-15 16:19:18 +0530

    Added Semntic functions and created console app

---
+++ SKWorkshop/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.SemanticKernel;
+
+
+// ... instantiating  kernel
+var builder = new KernelBuilder();
+
+builder.WithAzureTextCompletionService(
+            "", // Azure OpenAI Deployment Name
+            "", // Azure OpenAI Endpoint
+            "");      // Azure OpenAI Key
+
+var kernel = builder.Build();
+
+// Import Semantic Plugins
+var pluginsDirectory = Path.Combine("Path to your plugin directory");
+var orchestrationPlugin = kernel.ImportSemanticSkillFromDirectory(pluginsDirectory, "OrchestrationPlugin");
+
+// Create a new context and set the input, history, and options variables.
+var context = kernel.CreateNewContext();
+context["input"] = "I want to park my bike can you help me find an available slot?";
+context["options"] = "FetchAvailableParkingSlot, FindMyParkedVehicle, OutOfContext";
+
+// Run the GetIntent function with the context.
+var result = await orchestrationPlugin["GetIntent"].InvokeAsync(context);
+
+Console.WriteLine(result);
+
+var input = "User: I drive an EV car";
+var vehiclePlugin = kernel.ImportSemanticSkillFromDirectory(pluginsDirectory, "VehiclePlugin");
+var fuelResult = await vehiclePlugin["GetFuelType"].InvokeAsync(input);
+
+Console.WriteLine(fuelResult);
+
+var vehicleResult = await vehiclePlugin["GetVehicleType"].InvokeAsync(input);
+
+Console.WriteLine(vehicleResult);
+
+
+
